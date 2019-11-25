22b45c330625ebce9e68a359675ecf153c4d624e
Nandini <nandini@gmail.com>
2019-11-25 15:14:29 +0530

    Initial Commit

---
+++ src/app/mentor/mentorsignup/mentorsignup.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Mentor} from 'src/app/Models/mentor';
+import { MentorServiceService } from 'src/app/Services/mentor-service.service';
+
+@Component({
+  selector: 'app-mentorsignup',
+  templateUrl: './mentorsignup.component.html',
+  styleUrls: ['./mentorsignup.component.css']
+})
+export class MentorsignupComponent implements OnInit {
+  item:Mentor;
+  list:Mentor[];
+  msg:string;
+  
+  constructor(private _service : MentorServiceService) {
+    this.item= new Mentor();
+    this._service.GetAllMentors().subscribe(k=>this.list=k)
+   }
+  
+  ngOnInit() {
+  }
+  public Add()
+  {
+    this.item.mentor_Active=true;
+    this._service.AddMentor(this.item).subscribe(k=>this.msg=k);
+  }
+  public Delete()
+  {
+    this._service.DeleteMentor(this.item.mentor_Id).subscribe();
+  }
+  public Block()
+  {
+    this._service.BlockMentor(this.item.mentor_Id).subscribe();
+  }
+  
+}
